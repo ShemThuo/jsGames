1784bb0b64ffd25ac8e5efaa35ff51eeed8ea98e
Techflux <shemtmwangi@gmail.com>
2025-10-09 21:36:52 +0300

    JavaScript is Fun

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -48,27 +48,19 @@ const cardArray = [
         name: 'pizza', 
         img: 'images/pizza.jpg'
     },
-    {
-        name: 'bg', 
-        img: 'images/bg.jpg'
-    },
-    {
-        name: 'landscape', 
-        img: 'images/landscape.jpg'
-    },
-    {
-        name: 'lighthouse', 
-        img: 'images/lighthouse.jpg'
-    },
 ]
 
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
-const cardsChosen = []
-const cardsChosenIds = []
+const resultDisplay = document.querySelector('#result')
+const playAgainBtn = document.getElementById('play-again');
+let cardsChosen = []
+let cardsChosenIds = []
+let cardsWon = []
 
 function createBoard() {
+    gridDisplay.innerHTML = '';
     for (let i = 0; i<cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.jpg')
@@ -81,12 +73,53 @@ createBoard()
 
 function checkMatch () {
     const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    console.log(cards)
     console.log('check for a match!')
+    if (optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/blank.jpg')
+        alert('You have clicked the same image! Try again.');
+        cardsChosen = [];
+        cardsChosenIds = [];
+        return;
+    }
     if (cardsChosen[0] == cardsChosen[1]) {
         alert('Found a match!')
-        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
-        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.jpg')
+        cards[optionTwoId].setAttribute('src', 'images/blank.jpg')
+        alert('Sorry, try again!')
     }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length == (cardArray.length/2)) {
+        resultDisplay.textContent = 'Congrats 🎉🎉. You have found them all!!';
+        playAgainBtn.style.display = 'inline-block';
+    }
+// Play Again button logic
+if (playAgainBtn) {
+    playAgainBtn.addEventListener('click', () => {
+        // Reset state
+        cardsChosen = [];
+        cardsChosenIds = [];
+        cardsWon = [];
+        resultDisplay.textContent = '';
+        playAgainBtn.style.display = 'none';
+        // Shuffle cards
+        cardArray.sort(() => 0.5 - Math.random());
+        // Recreate board
+        createBoard();
+    });
+}
+
 }
 
 function flipCard() {
@@ -99,3 +132,4 @@ function flipCard() {
     }
 }
 
+//55:00 
